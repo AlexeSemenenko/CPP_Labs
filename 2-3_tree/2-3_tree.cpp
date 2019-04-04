4a6d968e18d0bd6f0a82c021b602bb8d49b58c99
Alexei <48149971+AlexeSemenenko@users.noreply.github.com>
2019-04-04 19:35:37 +0300

    Delete 2-3_tree.cpp

--- 2-3_tree/2-3_tree.cpp
+++
@@ -1,9 +0,0 @@
-#include "2-3_tree.h"
-#include <iostream>
-
-two_three_tree::two_three_tree()
-{
-	root_ = new two_three_node();
-	root_->child[0] = new two_three_node();
-	root_->child[0]->parent = root_;
-}
